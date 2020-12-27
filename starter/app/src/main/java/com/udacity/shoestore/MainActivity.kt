fe220de60e4763589300f1bc1a3b8ecc716cad6d
kevin.claassen <claassenk@outlook.com>
2020-12-27 16:12:56 +0100

    Removed findviewbyid in MainActivity.kt as per project requirements

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -6,12 +6,11 @@ import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.fragment_shoe_list.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +20,8 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         setOf(R.id.LoginFragment, R.id.welcomeFragment, R.id.instructionsFragment, R.id.shoeListFragment)
         setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<Toolbar>(R.id.toolbar)
-                .setupWithNavController(navController, appBarConfiguration)
-
-
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        
     }
 
 }
